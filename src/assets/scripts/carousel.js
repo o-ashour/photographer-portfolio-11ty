0cd02b78861359695495760348937a901f2ef9e1
shoegaze-86 <87482050+shoegaze-86@users.noreply.github.com>
2021-09-16 13:32:07 +0200

    Update carousel.js

--- src/assets/scripts/carousel.js
+++ src/assets/scripts/carousel.js
@@ -14,8 +14,9 @@ function currentSlide(n) {
 
 function SlideShow(n) {
   var i;
-  const slides[] = document.getElementsByClassName("Containers");
-  const circles[] = document.getElementsByClassName("dots");
+  const slides = [];
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
   if (n > slides.length) {slidePosition = 1}
   if (n < 1) {slidePosition = slides.length}
   for (i = 0; i < slides.length; i++) {
